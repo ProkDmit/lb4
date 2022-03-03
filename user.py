af79d2ec7cba5ad6e74c9803fadedc537d63fe40
ProkDmit <dmidtriypdv@gmail.com>
2022-03-03 17:04:57 +0300

    user.py

---
+++ user.py
@@ -0,0 +1,6 @@
+name = input("Your name?")
+old = input("How old are you?")
+address = input("Where do you live?")
+print("This is", name, ",")
+print("Who is", old, "years old")
+print("and lives in", address)
